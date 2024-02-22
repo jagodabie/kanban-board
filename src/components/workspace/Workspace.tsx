ab9e67c86342147f2404a24a24843f18a8a0faba
jagodabie <51316252+jagodabie@users.noreply.github.com>
2024-02-22 13:39:36 +0100

    refactor(): handle drag end

--- src/components/workspace/Workspace.tsx
+++ src/components/workspace/Workspace.tsx
@@ -1,22 +1,18 @@
 import { SortableContext } from '@dnd-kit/sortable';
 import { useAppDispatch, useAppSelector } from '../../hooks/useAppDispatch';
-import {
-  createWorkspaceTasksGroup,
-  setTasks,
-  setTasksGroupOrder,
-} from '../../store/slices/actions';
-import { changedElementsOrder, generateId, getTaskPosition } from '../../utils';
+import { createWorkspaceTasksGroup } from '../../store/slices/actions';
+import { generateId } from '../../utils';
 import { Input } from '../UI/input/Input';
 import './Workspace.scss';
 import {
   DndContext,
-  DragOverEvent,
   PointerSensor,
   closestCorners,
   useSensor,
   useSensors,
 } from '@dnd-kit/core';
 import { TasksGroups } from './tasksGroup/TasksGroup';
+import { useTasksGroupHandleDragEnd } from '../../hooks/useTasksGroupHandleDragEnd';
 
 export const Workspace = ({ id }: { id: string }) => {
   const dispatch = useAppDispatch();
@@ -34,79 +30,7 @@ export const Workspace = ({ id }: { id: string }) => {
       },
     })
   );
-  const handleDragEnd = (event: DragOverEvent) => {
-    const { active, over } = event;
-
-    if (active.id === over?.id) return;
-
-    const isActiveATask = active.data.current?.type === 'task';
-    const isOverATask = over?.data.current?.type === 'task';
-    const columnActiveId = active?.data.current?.element?.tasksGroupId || '';
-    const columnOverId = over?.data.current?.element?.tasksGroupId || '';
-    const activeTask = active.data.current?.element;
-
-    // DnD within in range  tasks group
-    if (isActiveATask && isOverATask && columnOverId === columnActiveId) {
-      const columnId = over?.data.current?.element.tasksGroupId || '';
-
-      const tasksGroupActive = tasksGroups.find(
-        (group) => group.id === columnId
-      );
-
-      dispatch(
-        setTasks({
-          tasksGroupId: columnId,
-          tasks: changedElementsOrder(
-            tasksGroupActive?.tasks || [],
-            getTaskPosition(tasksGroupActive?.tasks || [], active.id as string),
-            getTaskPosition(tasksGroupActive?.tasks || [], over.id as string)
-          ),
-        })
-      );
-    }
-    // DnD between columns
-    if (active.id && over?.id && !isOverATask && !isActiveATask) {
-      dispatch(
-        setTasksGroupOrder(
-          changedElementsOrder(
-            tasksGroups,
-            getTaskPosition(tasksGroups, active.id as string),
-            getTaskPosition(tasksGroups, over.id as string)
-          )
-        )
-      );
-    }
-    if (isActiveATask && isOverATask && columnOverId !== columnActiveId) {
-      const tasksGroupActive = tasksGroups.find(
-        (group) => group.id === columnActiveId
-      );
-
-      const tasksGroupOver =
-        tasksGroups.find((group) => group.id === columnOverId)?.tasks || [];
-
-      const newActiveTasks = tasksGroupActive?.tasks?.filter((task) => {
-        return task.id !== activeTask.id;
-      });
-
-      dispatch(
-        setTasks({
-          tasks: newActiveTasks?.length ? newActiveTasks : [],
-          tasksGroupId: columnActiveId,
-        })
-      );
-
-      dispatch(
-        setTasks({
-          tasks: [...tasksGroupOver, activeTask]?.length
-            ? [...tasksGroupOver, activeTask]
-            : [],
-          tasksGroupId: columnOverId,
-        })
-      );
-    }
-
-    return;
-  };
+  const handleDragEnd = useTasksGroupHandleDragEnd(tasksGroups);
 
   return (
     <div className='workspace'>
